42bdfe6c268939ef5588a4059c8e2f828107642d
Madhavi <Madhavi@DESKTOP-LTQH33R>
2022-11-03 19:24:19 +0530

    please yaar

---
+++ src/vaibhav/Test03.java
@@ -0,0 +1,24 @@
+package vaibhav;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test03 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.instagram.com/");
+
+		Thread.sleep(5000);
+		driver.findElement(By.name("username")).sendKeys("qwertyuio");
+		Thread.sleep(3000);
+		driver.findElement(By.name("password")).sendKeys("qwertyui");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("(//div[contains(@class,'qF0y9')])[4]")).click();
+
+	}
+
+}
